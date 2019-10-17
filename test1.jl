382a12e1b4edaf1982b2f53fbc6835ed18fac844
James Breedlove <qslick04@gmail.com>
2019-10-17 04:01:41 -0400

    Added test cases for BLOCK, WHILE, FOR, and IF

--- test1.jl
+++ test1.jl
@@ -1,5 +1,7 @@
 begin 
 
+	println("\n*******************************\n* Julia Test Program Starting *\n*******************************\n")
+
 	x = 1 
 	y = 2
 	z = 3
@@ -23,6 +25,34 @@ begin
 	print( x > y )
 	print( y < z )
 	print( y != z )
+
+	function test_RSVP_IF()
+		if(true == true)
+			println("PASS RSVP_IF: \"if\"")
+		end
+	end
+
+	function test_RSVP_WHIL()
+		while x < 5
+			println(x)
+			global x += x
+		end
+    end
+
+    function test_RSVP_FOR()
+		for i in [43, 3, 16]
+			println(i)
+		end
+    end
+
+	test_RSVP_IF()
+	println("PASS RSVP_IF: \"if\"")
+	test_RSVP_WHIL()
+	println("PASS RSVP_WHIL: \"while\"")
+	test_RSVP_FOR() 		
+	println("PASS RSVP_FOR: \"for\"")
+
 	
+    println("\nAll test complete")
 	
 end
